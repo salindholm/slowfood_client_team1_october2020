a58ecda8001dda8946becf4756ad929b0992d5b9
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-24 16:28:47 +0100

    Create userCanSeeListofProduct.feature.js, write test and add products.json

---
+++ cypress/integration/userCanSeeListofProducts.feature.js
@@ -0,0 +1,38 @@
+describe("User can view list of products", () => {
+  beforeEach(() => {
+    cy.visit("http://locslhost:3001");
+  });
+
+  describe("displays products", () => {
+    before(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/products",
+        response: "fixture:products.json",
+      });
+    });
+  });
+
+  it("successfully", () => {
+    cy.get("#index").within(() => {
+      cy.contains("Hawaii");
+      cy.contains("Margherita");
+      cy.contains("Vesuvio");
+    });
+  });
+});
+
+describe("displays no products", () => {
+  before(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/products",
+      response: [],
+    });
+  });
+  it("unsuccessfully", () => {
+    cy.get("#index").should("not.exist");
+  });
+});
