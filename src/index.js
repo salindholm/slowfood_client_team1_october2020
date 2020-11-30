49041f9c9810214ecad2555fefdce76331e6e901
Ehugo2000 <erik.hgsn@gmail.com>
2020-11-30 09:39:56 +0100

    Fixed baseURL in index.js with if statement

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import axios from "axios";
-import './assets/stylesheet.css'
+import "./assets/stylesheet.css";
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-axios.defaults.baseURL = "https://slowfood-revisarik-api.herokuapp.com/";
+let apiUrl;
+
+if (process.env.NODE_ENV === "production") {
+  apiUrl = "https://slowfood-revisarik-api.herokuapp.com/api/";
+} else {
+  apiUrl = "http://localhost:3000/api/";
+}
+axios.defaults.baseURL = apiUrl;
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
