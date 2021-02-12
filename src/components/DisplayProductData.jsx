515dc7cd6dec2b370e13d5f4c5482b30aa6a4743
Sanne <sannelindholm@hotmail.com>
2021-02-12 11:03:00 +0100

    Removing comments

--- src/components/DisplayProductData.jsx
+++ src/components/DisplayProductData.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 
 class DisplayProductData extends Component {
-	// state = {
-	// 	productData: [],
-	// };
-
-
+	state = {
+		productData: [],
+	};
 
 	render() {
 		let dataIndex;
