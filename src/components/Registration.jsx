06b50afbc145121bb3620217355df34f00fe86b9
Sanne <sannelindholm@hotmail.com>
2021-03-01 11:23:21 +0100

    Adding input fields with submit button to form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Registration = () => {
+  const dispatch = useDispatch()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const [passwordConfirmation, setPasswordConfirmation] = useState()
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <form>
+        <input type='email' placeholder='Email' data-cy='register-email' />
+        <input
+          type='password'
+          placeholder='Password'
+          data-cy='register-password'
+        />
+        <input
+          type='password'
+          placeholder='Password confirmation'
+          data-cy='register-password-confirmation'
+        />
+        <input type='button' value='Submit' data-cy='submit' />
+      </form>
+    </>
   )
 }
 
