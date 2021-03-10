508fcf75f0fe608621f2ef4dc909e5ade358ccf4
Sanne <sannelindholm@hotmail.com>
2021-03-10 12:08:38 +0100

    Adding logic for items count and responsive order message

--- src/components/DisplayProducts.jsx
+++ src/components/DisplayProducts.jsx
@@ -1,50 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { productService } from '../modules/productService'
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { productService } from '../modules/productService';
+import axios from 'axios';
 
 const DisplayProducts = () => {
-  const dispatch = useDispatch()
-  const { productsFeed } = useSelector((state) => state)
-  const [orderMessage, setOrderMessage] = useState()
-  const credentials = useSelector(state => state.credentials)
+  const dispatch = useDispatch();
+  const { productsFeed } = useSelector((state) => state);
+  const [orderMessage, setOrderMessage] = useState();
+  const [orderID, setOrderID] = useState();
+  const [itemsCountMessage, setItemsCountMessage] = useState();
+  const credentials = useSelector((state) => state.credentials);
 
   useEffect(() => {
-    productService.index(dispatch)
-  }, [dispatch])
+    productService.index(dispatch);
+  }, [dispatch]);
 
-  const addToOrder = async (productID) => {
-    const response = await axios.post(
-      '/orders',
-      { product_id: productID },
-      { headers: credentials },
-    ) 
-    setOrderMessage(response.data.message)
-  }
-
+  const addToOrder = async (productID, productName) => {
+    if (orderID) {
+      let response = await axios.put(
+        '/orders',
+        { product_id: productID },
+        { headers: credentials }
+      );
+      setItemsCountMessage(
+        `You have ${response.data.items.lenght} items in your order`
+      );
+      setOrderMessage(`${response.data.message} (1 x ${productName})`);
+    } else {
+      let response = await axios.post(
+        '/orders',
+        { product_id: productID },
+        { headers: credentials }
+      );
+      setOrderID(response.data.items[0].order_id);
+      setItemsCountMessage(`You have 1 item in your order.`);
+      setOrderMessage(`${response.data.message} (1 × ${productName})`);
+    }
+  };
   return (
     <>
-    <div data-cy='products-index'>
-      {productsFeed &&
-        productsFeed.map((product) => {
-          return (
-            <div key={product.id}
-            data-cy={`product-${product.id}`}>
-              {product.name}
-              {product.description}
-              {product.price}
-              <button
-                data-cy={`add-to-order-button${product.id}`}
-                onClick={() => addToOrder(product.id)}
-              >
-                Add To Order
-              </button>
-            </div> )
-        })}
-    </div>
-       <p data-cy="order-message">{orderMessage}</p>
-       </>
-  )
-}
+      <div data-cy='products-index'>
+        {productsFeed &&
+          productsFeed.map((product) => {
+            return (
+              <div key={product.id} data-cy={`product-${product.id}`}>
+                {product.name}
+                {product.description}
+                {product.price}
+                <button
+                  data-cy={`add-to-order-button${product.id}`}
+                  onClick={() => addToOrder(product.id, product.name)}
+                >
+                  Add To Order
+                </button>
+              </div>
+            );
+          })}
+      </div>
+      <p data-cy='order-message'>{orderMessage}</p>
+      <p data-cy='items-count-message'>{itemsCountMessage}</p>
+    </>
+  );
+};
 
-export default DisplayProducts
+export default DisplayProducts;
--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,62 +1,65 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import axios from 'axios'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
 const Registration = () => {
-  const dispatch = useDispatch()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-  const [passwordConfirmation, setPasswordConfirmation] = useState()
-  const userID = useSelector((state) => state.credentials.uid)
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordConfirmation, setPasswordConfirmation] = useState();
+  const userID = useSelector((state) => state.credentials.uid);
 
   const userRegistration = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const response = await axios.post('/auth', {
       email: email,
       password: password,
       password_confirmation: passwordConfirmation,
-    })
+    });
     const credentials = {
       uid: response.headers['uid'],
       access_token: response.headers['access-token'],
       token_type: response.headers['token-type'],
       expiry: response.headers['expiry'],
       client: response.headers['client'],
-    }
-    dispatch({ type: 'SET_CURRENT_USER', payload: credentials })
-  }
+    };
+    dispatch({ type: 'SET_CURRENT_USER', payload: credentials });
+  };
 
   return (
     <>
-      <form>
-        <input
-          type='email'
-          placeholder='Email'
-          data-cy='register-email'
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          data-cy='register-password'
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <input
-          type='password'
-          placeholder='Password confirmation'
-          data-cy='register-password-confirmation'
-          onChange={(event) => setPasswordConfirmation(event.target.value)}
-        />
-        <input
-          type='button'
-          value='Submit'
-          data-cy='submit'
-          onClick={userRegistration}
-        />
-      </form>
-      {userID && <p data-cy='register-message'>Welcome {userID}</p>}
+      {!userID ? (
+        <form>
+          <input
+            type='email'
+            placeholder='Email'
+            data-cy='register-email'
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type='password'
+            placeholder='Password'
+            data-cy='register-password'
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <input
+            type='password'
+            placeholder='Password confirmation'
+            data-cy='register-password-confirmation'
+            onChange={(event) => setPasswordConfirmation(event.target.value)}
+          />
+          <input
+            type='button'
+            value='Submit'
+            data-cy='submit'
+            onClick={userRegistration}
+          />
+        </form>
+      ) : (
+        <p data-cy='register-message'>Welcome {userID}</p>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Registration
+export default Registration;
